823018669e06c9a6f1b249a9c8c50d8eddfb5ae4
Белолипецкий Владислав Александрович <ip613s05@mwp12.csc.local>
2017-03-09 09:05:58 +0700

    Chast 2

--- lab2.c
+++ lab2.c
@@ -3,9 +3,32 @@
 int main()
 {
   int sum,srok;
-  cout <<"Vvedite srok i sumu";
+  cout <<"Vvedite srok i summu";
   cin >> srok >> sum;
   if (srok > 365; srok < 0; sum < 100){
     cout <<"Nekorektno vvedni danie";
   } else {
-    
+    if (srok < 31){
+      sum = sum-((sum/100)*10);
+      cout <<"Summa ravna = "<< sum;
+    } else {
+      if (srok < 121){
+	sum = sum+((sum/100)*2);
+        cout <<"Summa ravna = "<< sum;
+      } else {
+	if (srok < 241){
+	  sum = sum+((sum/100)*6);
+	  cout <<"Summa ravna = "<< sum;
+	} else {
+	    sum = sum-((sum/100)*10);
+	    cout <<"Summa ravna = "<< sum;
+	  }
+      }
+    }
+  }
+  return 0;
+}
+      
+      
+	 
+      
