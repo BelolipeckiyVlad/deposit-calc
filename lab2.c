1798f608232c81f7bf6e350dfc36b75e05fa6072
Белолипецкий Владислав Александрович <ip613s05@mwp12.csc.local>
2017-03-09 08:55:19 +0700

    1 chast programmi

---
+++ lab2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+int main()
+{
+  int sum,srok;
+  cout <<"Vvedite srok i sumu";
+  cin >> srok >> sum;
+  if (srok > 365; srok < 0; sum < 100){
+    cout <<"Nekorektno vvedni danie";
+  } else {
+    
