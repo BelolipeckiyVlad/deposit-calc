667c898071e966792aeaa9a5b666272f24ddcbfe
Белолипецкий Владислав Александрович <ip613s05@mwp07.csc.local>
2017-03-16 08:47:55 +0700

    pravka ohibok#2

--- lab2.c
+++ lab2.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
-#include <iostream>
 using namespace std;
 int main()
 {
-  int sum,srok;
-  cout <<"Vvedite srok i summu";
-  cin >> srok >> sum;
-  if (srok > 365; srok < 0; sum < 100){
-    cout <<"Nekorektno vvedni danie";
+  int sum,time;
+  printf("Vvedite srok i summu");
+  scanf("%d %d",&time,&sum); 
+  if (time > 365; time < 0; sum < 100){
+    printf ("Nekorektno vvedni danie");
   } else {
-    if (srok < 31){
+    if (time < 31){
       sum = sum-((sum/100)*10);
-      cout <<"Summa ravna = "<< sum;
+      printf ("Summa ravna =%d",sum);
     } else {
-      if (srok < 121){
-	sum = sum+((sum/100)*2);
-        cout <<"Summa ravna = "<< sum;
+      if (time < 121){
+        sum = sum+((sum/100)*2);
+        printf ("Summa ravna =%d",sum);
       } else {
-	if (srok < 241){
+	if (time < 241){
 	  sum = sum+((sum/100)*6);
-	  cout <<"Summa ravna = "<< sum;
+	  printf ("Summa ravna =%d", sum);
 	} else {
 	    sum = sum-((sum/100)*10);
-	    cout <<"Summa ravna = "<< sum;
+	    printf ("Summa ravna =%d", sum);
 	  }
       }
     }
