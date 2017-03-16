f1b5e6ae8bcde96e51771c0fdf0269bc93bec238
Белолипецкий Владислав Александрович <ip613s05@mwp07.csc.local>
2017-03-16 09:06:16 +0700

    pravka ohibok#4

--- lab2.c
+++ lab2.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 int main()
 {
-  int sum,time;
+  int sum, time;
   printf("Vvedite srok i summu");
-  scanf("%d %d",&time,&sum); 
-  if (time > 365, time < 0, sum < 100){
+  scanf("%d %d", &time, &sum); 
+  if (time > 365 && time < 0 && sum < 1000){
     printf ("Nekorektno vvedni danie");
   } else {
     if (time < 31){
-      sum = sum-((sum/100)*10);
-      printf ("Summa ravna =%d",sum);
+      sum = sum - ((sum / 100) * 10);
+      printf ("Summa ravna =%d", sum);
     } else {
       if (time < 121){
-        sum = sum+((sum/100)*2);
-        printf ("Summa ravna =%d",sum);
+        sum = sum + ((sum / 100) * 2);
+        printf("Summa ravna =%d", sum);
       } else {
 	if (time < 241){
-	  sum = sum+((sum/100)*6);
-	  printf ("Summa ravna =%d", sum);
+	  sum = sum + ((sum / 100) * 6);
+	  printf("Summa ravna =%d", sum);
 	} else {
-	    sum = sum-((sum/100)*10);
-	    printf ("Summa ravna =%d", sum);
+	    sum = sum - ((sum / 100) * 10);
+	    printf("Summa ravna =%d", sum);
 	  }
       }
     }
